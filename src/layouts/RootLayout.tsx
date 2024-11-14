87f4247eb4ed7416193ec8924d6c80f0a53ce55d
AxonDesigns <acciol400@gmail.com>
2024-11-14 07:36:47 -0500

    Update RootLayout.tsx

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -4,14 +4,15 @@ import { Outlet } from 'react-router-dom'
 function RootLayout() {
   const { isLoading } = useAuth();
 
-  if (isLoading) {
-    return <div className='h-dvh flex items-center justify-center'>
-      <img src="/favicon.svg" className="w-32 h-32" />
-    </div>
-  }
-
   return (
-    <Outlet />
+    <>
+      <Outlet />
+      {isLoading && (
+        <div className='absolute top-0 left-0 w-full h-dvh flex items-center justify-center'>
+          <img src="/favicon.svg" className="w-32 h-32" />
+        </div>
+      )}
+    </>
   )
 }
 
